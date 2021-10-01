c188b1e02b07f8cf9bf103a79aa0567648e1663e
Brooke Wittenberg <bwittenberg.developer@gmail.com>
2021-10-01 15:43:28 -0500

    calculator working

---
+++ src/__tests__/calc.test.js
@@ -0,0 +1,9 @@
+const calc = (pricipal, interest, years) => {
+    callback(a + b);
+};
+
+test("calls callback with arguments added", () => {
+    const mockCallback = jest.fn();
+    doAdd(1, 2, mockCallback);
+    expect(mockCallback).toHaveBeenCalledWith(3);
+});
